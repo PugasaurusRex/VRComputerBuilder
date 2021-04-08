08d94346e426947895c8077c6d71e112ed5d62ce
Mark Ellis <mellis6464@floridapoly.edu>
2021-04-08 11:30:39 -0400

    First Commit

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public bool beenUsed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += SceneChanger;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void SceneChanger(Scene s, LoadSceneMode m)
+    {
+        if(beenUsed)
+        {
+            SceneManager.sceneLoaded -= SceneChanger;
+            Destroy(this.gameObject);
+        }
+        if (s.name != SceneManager.GetSceneByBuildIndex(0).name)
+        {
+            beenUsed = true;
+        }
+    }
+
+    public void switchScenes()
+    {
+        if (!beenUsed)
+        {
+            Debug.Log("Loading game scene");
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            Debug.Log("Loading Menu scene");
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/Scripts/ToggleText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleText : MonoBehaviour
+{
+    public bool isVisible = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Toggle(GameObject text)
+    {
+        if(isVisible)
+        {
+            isVisible = false;
+            text.gameObject.SetActive(false);
+        }
+        else
+        {
+            isVisible = true;
+            text.gameObject.SetActive(true);
+        }
+    }
+}
